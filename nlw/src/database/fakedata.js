569f293e9c0ae46d912243abff738c2b2ed92e03
Rafael Vendramini <72450490+rafaelbvc@users.noreply.github.com>
2020-10-16 02:18:48 -0300

    Add files via upload

---
+++ nlw/src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Lar das meninas",
+        lat: "-27.222633", 
+        lng: "-49.6455874",
+        description: "Presta assistencia bla bla bla blâ blá",
+        images: [
+            "https://images.unsplash.com/photo-1590966473477-e74b95a0c407?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            
+            "https://images.unsplash.com/photo-1590966473477-e74b95a0c407?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 08h até 18h",
+        open_on_weekends: "1",
+    },
+    {
+        id: 2,
+        name: "Lar dos menios",
+        lat: "-27.222633", 
+        lng: "-49.6555874",
+        description: "Presta assistencia bla bla bla blâ blá",
+        images: [
+            "https://images.unsplash.com/photo-1590966473477-e74b95a0c407?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            
+            "https://images.unsplash.com/photo-1590966473477-e74b95a0c407?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 08h até 18h",
+        open_on_weekends: "0",
+    },
+]
